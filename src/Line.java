bd6fd58a9b40bbe75798fcde216a89c68670e495
gvb19190 <taaphoo1Eof9>
2022-02-16 14:20:47 +0000

    Made station and line class implementing a linked list

---
+++ src/Line.java
@@ -0,0 +1,15 @@
+package src;
+
+public class Line {
+
+    public static void main(String[] args) {
+        String line = "line1";
+        Station station1 = new Station("one", line);
+        Station station2 = new Station("two", line);
+        Station station3 = new Station("three", line);
+        station1.setNext(station2);
+        station2.setPreviousNext(station1, station3);
+        station3.setPrevious(station2);
+    }
+
+}
---
+++ src/Station.java
@@ -0,0 +1,38 @@
+package src;
+
+public class Station {
+
+    Station previous;
+    Station next;
+    String name;
+    String line;
+
+    public Station(String name, String line) {
+        this.name = name;
+        this.line = line;
+    }
+
+    public void Station(Station previous, Station next, String name, String line) {
+        this.previous = previous;
+        this.next = next;
+        this.name = name;
+        this.line = line;
+    }
+
+    public void setPreviousNext(Station previous, Station next){
+        this.previous = previous;
+        this.next = next;
+    }
+
+    public Station getPrevious() { return previous; }
+
+    public void setPrevious(Station previous) { this.previous = previous; }
+
+    public Station getNext() { return next; }
+
+    public void setNext(Station next) { this.next = next; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+}
