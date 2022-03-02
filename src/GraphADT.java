0e7ddc7a85e488757f014a19a9a3c03cb8241e5a
psb18130 <lewis.danby.2018@uni.strath.ac.uk>
2022-03-02 12:35:01 +0000

    start adt

---
+++ src/GraphADT.java
@@ -0,0 +1,14 @@
+public class GraphADT {
+
+    int numVertices;
+
+    public GraphADT(int v) {
+        this.numVertices = v;
+    }
+
+    public void makeGraph(collection adjacencys) {
+
+    }
+
+
+}
