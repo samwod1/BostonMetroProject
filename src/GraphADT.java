dcc9208e49954174bd4db4f9950a8b7a10113c92
psb18130 <50241330+Danby3000@users.noreply.github.com>
2022-03-04 12:29:21 +0000

    adjlist Graph methods for ints

--- src/GraphADT.java
+++ src/GraphADT.java
@@ -3,6 +3,9 @@ import java.util.*;
 
 public class GraphADT {
 
+    List<List<Integer>>adjList;
+    private int numNodes;
+
     ArrayList vertices = new ArrayList();
     ArrayList<ArrayList> edges = new ArrayList();
 
@@ -10,7 +13,76 @@ public class GraphADT {
         this.vertices = vertices;
         this.edges = edges;
     }
+    //keeping these constructors seperate for the time being but we might merge them
 
+    public GraphADT(int n) {
+        this.numNodes = n;
+        adjList = new ArrayList<>(numNodes);
+        for (int i = 0; i < numNodes; i++) {
+            adjList.add(new ArrayList<>());
+        }
+    }
+
+    public int nNodes()
+    {
+        return numNodes;
+    }
+
+    public int nEdges()
+    {
+        int count = 0;
+        for(int i = 0; i < numNodes; i++) {
+            int n = adjList.size();
+            for(int j = 0; j < n; j++) {
+                int a = adjList.get(i).get(j);
+                if(a > i){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public boolean isEdge(int node1, int node2) {
+        int i=0;
+        boolean found = false;
+        while (!found && i<nNodes())
+        {
+            if (adjList.get(node1).contains(node2))
+                found = true;
+            else
+                i++;
+        }
+        return found;
+
+    }
+
+    public boolean addEdge(int node1, int node2) {
+
+        if (isEdge(node1 ,node2)) {
+            return false;
+        }
+
+        adjList.get(node1).add(node2);
+        adjList.get(node2).add(node1);
+        return true;
+
+    }
+
+    public ArrayList<Integer> successors(int node)
+    {
+        ArrayList<Integer> successorNodes = new ArrayList<Integer>();
+        for (int i=0; i<nNodes(); i++)
+            if (adjList.get(node).contains(i))
+                successorNodes.add(i);
+        return successorNodes;
+    };
+
+    public int outDegree(int node)
+    {
+        return successors(node).size();
+    }
+    
     //create construct graph method which takes in input and crates adjacency list.
     //create other methods which add other functionality
 
