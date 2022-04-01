27832cf4a6811e7f0018943f146572484d22dc97
psb18130 <lewis.danby.2018@uni.strath.ac.uk>
2022-04-01 16:33:24 +0100

    began thinking of doing Route

--- src/Route.java
+++ src/Route.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+
 public class Route {
 
     public Route makeRoute(Station n1, Station n2) {
+        //find lines of each station
+        ArrayList linesn1 = n1.getLines();
+        ArrayList linesn2 = n2.getLines();
 
+        //send those lines to graph to find route
 
         return Route;
     }
