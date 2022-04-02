70a07ff0607b18549d20e3a625109d14add95cde
psb18130 <50241330+Danby3000@users.noreply.github.com>
2022-04-02 19:15:06 +0100

    created closestNodeStation

--- src/Route.java
+++ src/Route.java
@@ -7,8 +7,44 @@ public class Route {
         ArrayList linesn1 = n1.getLines();
         ArrayList linesn2 = n2.getLines();
 
+        boolean sameLine = false;
+
+        Station connect1;
+        Station connect2;
+
+        //checking if start and end are nodes
+        if(linesn1.size() > 1) {
+            connect1 = n1;
+        }
+
+        if(linesn2.size() > 1) {
+            connect2 = n2;
+        }
+
+        //find if they share a line
+        for (int i = 0; i < linesn1.size(); i++) {
+            if(linesn2.contains(linesn1.get(i))) {
+                sameLine = true;
+            }
+        }
+
         //send those lines to graph to find route
 
-        return Route;
+
+        return path;
     }
+
+    public Station closestNodeStation(Station s) {
+        ArrayList cons = s.getConnections();
+        Station next = (Station) cons.get(0);
+        int nextsize = (next.getLines()).size();
+
+        if (nextsize > 1) {
+            return next;
+        } else {
+            closestNodeStation(next);
+        }
+        return null;
+    }
+
 }
