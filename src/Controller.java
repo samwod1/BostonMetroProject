926b3d555e7c06575b2c1d99f8c0f37d47be9325
psb18130 <lewis.danby.2018@uni.strath.ac.uk>
2022-04-03 16:01:16 +0100

    stations getNumberAsint fix

--- src/Controller.java
+++ src/Controller.java
@@ -2,8 +2,8 @@ public class Controller {
 
     //need to make a function to tell when there's a change of line
 
-    public Station stnfromstr(String stn){
-
-    }
+//    public Station stnfromstr(String stn){
+//
+//    }
 
 }
--- src/main.java
+++ src/main.java
@@ -1,13 +1,24 @@
-
+import java.util.ArrayList;
 
 public class main {
 
     public static void main(String[] args) {
-        //InputReader inputReader = new InputReader();
-        //inputReader.readFile();
+        InputReader inputReader = new InputReader();
+        inputReader.readFile();
         NetworkConstructor networkConstructor = new NetworkConstructor();
-        //networkConstructor.createStations();
+        ArrayList<Station> stations = networkConstructor.createStations();
         networkConstructor.connections();
+        Route route = new Route();
+        System.out.println(stations.get(3));
+        System.out.println(stations.get(5));
+
+        Station revbeach = stations.get(3);
+        Station beachm = stations.get(5);
+
+        System.out.println(route.makeRoute(revbeach, beachm));
+
+
+
     }
 
 }
