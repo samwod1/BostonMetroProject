b6db95219bedc960eb3d387248d73ee04ec0d5a2
psb18130 <lewis.danby.2018@uni.strath.ac.uk>
2022-04-08 11:38:28 +0100

    tests for NetworkConstructor

--- test/testNetworkConstructor.java
+++ test/testNetworkConstructor.java
@@ -4,6 +4,30 @@ import static org.junit.Assert.*;
 
 public class testNetworkConstructor {
 
+    @Test
+    public void testcreateStations() {
+        ArrayList<Station> stations = NetworkConstructor.createStations();
+        Integer len = stations.size();
+        Integer numstations = 124;
 
+        assertEquals(len, numstations);
+    }
+
+
+    @Test
+    public void teststnfromStr() {
+        Station a = NetworkConstructor.stnfromStr("OakGrove");
+        Station b = NetworkConstructor.getStnFromInt(1);
+
+        assertEquals(a,b);
+    }
+
+    @Test
+    public void testgetStnFromInt() {
+        Station a = NetworkConstructor.getStnFromInt(1);
+        Station b = NetworkConstructor.stnfromStr("OakGrove");
+
+        assertEquals(a,b);
+    }
 
 }
